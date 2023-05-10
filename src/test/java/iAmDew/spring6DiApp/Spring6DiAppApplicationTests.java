c815c6fff47e79f3cd33f4d3b7ddd332b7823df6
Rasa <rasa.kajackaite@gmail.com>
2023-05-10 11:29:15 +0300

    context test

--- src/test/java/iAmDew/spring6DiApp/Spring6DiAppApplicationTests.java
+++ src/test/java/iAmDew/spring6DiApp/Spring6DiAppApplicationTests.java
@@ -1,10 +1,32 @@
 package iAmDew.spring6DiApp;
 
+import iAmDew.spring6DiApp.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiAppApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	// two different ways to do the same:
+	@Test
+	void testAutowiredOfController() {
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
